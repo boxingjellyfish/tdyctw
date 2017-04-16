372f2c8d0395e508d11f6d0c21f90931a6354958
tutethedude <tute1985@gmail.com>
2017-04-16 09:34:45 -0300

    db connection

--- tdyctw/Controllers/HomeController.cs
+++ tdyctw/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Dapper;
+using System.Data.SqlClient;
 
 namespace tdyctw.Controllers
 {
@@ -11,6 +14,12 @@ namespace tdyctw.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            using (var conn = new SqlConnection(ConfigurationManager.ConnectionStrings["tdyctwdb"].ConnectionString))
+            {
+                conn.Open();
+                var result = conn.Query<dynamic>("select paramkey, paramvalue from parameter where paramkey = @ParamKey", new { ParamKey = "appversion" });
+                ViewBag.AppVersion = result.FirstOrDefault().paramvalue;
+            }
             return View();
         }
     }
